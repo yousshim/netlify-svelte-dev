b773a2e2373551d2b8b7ef180934b276daa0b6bf
yousef ibrahim <yousef98ibrahim@gmail.com>
2021-01-15 20:19:31 +0200

    hello api route returns random names with cache header max-age=300,s-max-age=600

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,14 @@
+const faker = require("faker");
+
 exports.handler = async function (event, context) {
+  console.log("running function")
+  const name = faker.name.firstName();
+  console.log(`generated name is: ${name}`);
   return {
     statusCode: 200,
-    body: JSON.stringify({ name: "world" })
-  }
+    headers: {
+      "Cache-Contorl": "max-age=300,s-max-age=600",
+    },
+    body: JSON.stringify({ name })
+  };
 }
